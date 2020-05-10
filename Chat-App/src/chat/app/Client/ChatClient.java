e5cdc36d1049e0b0a791b05e74b8628456afe7bb
tunchasan <hasantunc_ogr@outlook.com>
2020-05-10 08:11:45 +0300

    Added server command list usage examples as text

--- Chat-App/src/chat/app/Client/ChatClient.java
+++ Chat-App/src/chat/app/Client/ChatClient.java
@@ -18,14 +18,14 @@ public class ChatClient {
     
     public static void main(String[] args) throws Exception {
         
-        System.out.println( "Server Action List"
-                                 + "\n"
-                                 + "1- Send message to a user\n"
-                                 + "2- Send message to a group\n"
-                                 + "3- Send message to all user\n"
-                                 + "***************************");
+        System.out.println( "Server Command List\n"
+                                 + "/singleUser + {Username} + {Message}  ---> Send message to a user\n"
+                                 + "/group + {Message} ----------------------> Send message to a group\n"
+                                 + "/allUser + {Message} --------------------> Send message to all user\n"
+                                 + "/quit -----------------------------------> Close the connection to the server\n"
+                                 + "********************************************************************************************");
         
-        try (var socket = new Socket("localhost", SERVER_PORT_NO)) {
+         try (var socket = new Socket("localhost", SERVER_PORT_NO)) {
             
             System.out.println("Enter an user name:");
             
