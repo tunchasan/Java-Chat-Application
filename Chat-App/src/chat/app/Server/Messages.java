5666f659113bdc480d29b4a977543e7f9bc65c3b
tunchasan <hasantunc_ogr@outlook.com>
2020-05-11 00:05:59 +0300

    Added new result message to Message class' enum

--- Chat-App/src/chat/app/Server/Messages.java
+++ Chat-App/src/chat/app/Server/Messages.java
@@ -3,4 +3,9 @@ package chat.app.Server;
 // The class handles all client-server request result messages
 public class Messages {
     
+    enum Results {
+        Message_successfully_sended,
+        Message_send_failure,
+        User_not_exist,
+    }
 }
