718553639b6259b8ff862a0d2f08f72bb162f5c2
tunchasan <hasantunc_ogr@outlook.com>
2020-05-11 08:29:28 +0300

    Fixed client /quit command error

--- Chat-App/src/chat/app/Client/ChatClientReadHandler.java
+++ Chat-App/src/chat/app/Client/ChatClientReadHandler.java
@@ -2,25 +2,36 @@ package chat.app.Client;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.System.exit;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ChatClientReadHandler implements Runnable {
     
     private Socket socket;
     private Scanner in;
+    private ChatUI chatUI;
             
-    public ChatClientReadHandler(Socket socket) throws IOException{
+    public ChatClientReadHandler(Socket socket, ChatUI chatUI) throws IOException{
         this.socket = socket;
         in = new Scanner(this.socket.getInputStream());
+        this.chatUI = chatUI;
     }
     
     @Override
     public void run() {
         String serverResponse = null;
-        while (true){
-                serverResponse = in.nextLine();
-                System.out.println(" > " + serverResponse);
-        }
+            try{
+                while (true){
+                    serverResponse = in.nextLine();
+                    chatUI.AddMessageToUI(" > " + serverResponse);
+                    System.out.println(" > " + serverResponse);
+            }
+        } catch (NoSuchElementException e) { }
+            finally {
+                chatUI.RemoveUI();
+                exit(1);
+         }         
     }
 }
