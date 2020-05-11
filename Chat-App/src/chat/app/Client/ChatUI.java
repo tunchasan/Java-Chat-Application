e0cbe9b78fb9aba8cf240f9d07a6d2a42e3ab3c7
tunchasan <hasantunc_ogr@outlook.com>
2020-05-11 08:30:49 +0300

    Added initial text to textField and fixed getting input via panel issue

--- Chat-App/src/chat/app/Client/ChatUI.java
+++ Chat-App/src/chat/app/Client/ChatUI.java
@@ -1,6 +1,8 @@
 package chat.app.Client;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.io.PrintWriter;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -11,27 +13,46 @@ public class ChatUI {
     private JFrame frame;
     private JTextField textField;
     private JTextArea messageArea;
-    private String userName = "";
     
-    public ChatUI(String name){
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame = new JFrame("Chatter");
+    private PrintWriter out;
+    
+    public ChatUI(PrintWriter writer){
+        frame = new JFrame("JavaChats");
         textField = new JTextField(50);
         messageArea = new JTextArea(16, 50);
-        userName = name;
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        this.out = writer;
+        
         // Configurate Chat UI
         ConfigureUI();
+        
+        ConfigureInitialText();
     }
-    
+
     public void ConfigureUI(){
-        textField.setEditable(false);
+        textField.setEditable(true);
         messageArea.setEditable(false);
         frame.getContentPane().add(textField, BorderLayout.SOUTH);
         frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.CENTER);
         frame.pack();
-        //Set frame title
-        this.frame.setTitle("Chatter - " + userName);
+        
+        // Send on enter then clear to prepare for next message
+        textField.addActionListener((ActionEvent e) -> {
+            out.println(textField.getText());
+            textField.setText("");
+
+        });
+    }
+    
+    public void ConfigureInitialText (){
+        messageArea.append("Server Command List\n"
+                                    + "/createGroup -----> Create a new group\n"
+                                    + "/singleUser -------> Send message to a user\n"
+                                    + "/group --------------> Send message to a group\n"
+                                    + "/allUser ------------> Send message to all user\n"
+                                    + "/quit -----------------> Close the connection to the server\n"
+                                    + "***************************************************************\n\n");
     }
     
     public void RemoveUI(){
