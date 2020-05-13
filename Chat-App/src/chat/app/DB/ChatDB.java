6f04c7ce79a1971381f97cdcf55cd6d29a5defeb
tunchasan <hasantunc_ogr@outlook.com>
2020-05-13 19:03:40 +0300

    Mysql connection script has been added

--- Chat-App/src/chat/app/DB/ChatDB.java
+++ Chat-App/src/chat/app/DB/ChatDB.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package chat.app.DB;
 
-/**
- *
- * @author ganov
- */
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ChatDB {
     
-}
+    public ChatDB() {
+        //Connect database
+        try{  
+                Class.forName("com.mysql.jdbc.Driver");  
+                Connection con = (Connection) DriverManager.getConnection(  
+                "jdbc:mysql://localhost:3306/chatappdb","root","Mrrobot5444");  
+                 System.out.println("Database connection success");
+                } catch(ClassNotFoundException | SQLException e) { System.out.println(e);}  
+         } 
+    
+    }
+
