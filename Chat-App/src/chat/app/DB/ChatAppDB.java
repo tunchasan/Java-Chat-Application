fb6c01ffe258939c4b09d9cbcdfd1ab28b7c6c94
tunchasan <hasantunc_ogr@outlook.com>
2020-05-13 20:19:16 +0300

    Added a few method to handle server-database data communication

--- Chat-App/src/chat/app/DB/ChatAppDB.java
+++ Chat-App/src/chat/app/DB/ChatAppDB.java
@@ -2,6 +2,7 @@
 package chat.app.DB;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,11 +10,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ChatDB {
-    
+public class ChatAppDB {
+
     private Connection con;
     
-    public ChatDB() {
+    public ChatAppDB() {
         //Connect database
         try{  
                 Class.forName("com.mysql.jdbc.Driver");  
@@ -22,11 +23,7 @@ public class ChatDB {
                  System.out.println("Database connection success");
                 } catch(ClassNotFoundException | SQLException e) { System.out.println(e);}  
          } 
-    
-    public void Create(){
-        
-    }
-    
+
     public List<String> GetUserList() throws SQLException{
         String sql = "SELECT * FROM userlist";
         Statement statement = (Statement) con.createStatement();
@@ -35,28 +32,52 @@ public class ChatDB {
         List<String> resultList = new ArrayList<String>();
         
         while(result.next()) {
-            String username = result.getString(1);
-            resultList.add(username);
+            resultList.add(result.getString(1));
         }
 
         return resultList;
     }
     
-    public void Update(){
+     public List<MessageDB> GetMessageList() throws SQLException{
+        String sql = "SELECT * FROM messageList";
+        Statement statement = (Statement) con.createStatement();
+        ResultSet result = statement.executeQuery(sql);
         
+        List<MessageDB> resultList = new ArrayList<MessageDB>();
+        
+        while(result.next()) {
+            resultList.add(new MessageDB(result.getString(1), result.getString(2), result.getString(3), result.getString(4)));
+        }
+
+        return resultList;
     }
     
-    public void Delete(){
-        
+    public void InsertToUserList(String object) throws SQLException{
+        String sql = "INSERT INTO userlist (username) VALUES (?)";
+        PreparedStatement statement = (PreparedStatement) con.prepareStatement(sql);
+        statement.setString(1, object);
+        statement.executeUpdate();
+    }
+    
+    public void InsertToMessageList(MessageDB object) throws SQLException{
+        String sql = "INSERT INTO messagelist (sender, message, time, receiver) VALUES (?, ?, ?, ?)";
+        PreparedStatement statement = (PreparedStatement) con.prepareStatement(sql);
+        statement.setString(1, object.getSender());
+        statement.setString(2, object.getMessage());
+        statement.setString(3, object.getTime());
+        statement.setString(4, object.getReceiver());
+        statement.executeUpdate();
     }
     
     public static void main(String[] args) throws SQLException{
-        ChatDB chat = new ChatDB();
+        ChatAppDB chat = new ChatAppDB();
         
-        List<String> resultList = chat.GetUserList();
-
-        for(String x : resultList){
-            System.out.println(x);
+        List<MessageDB> resultList = chat.GetMessageList();
+        MessageDB sa = new MessageDB("ADSD","DSA","DASAS","DSA");
+        chat.InsertToUserList("HasanD");
+        chat.InsertToMessageList(sa);
+        for(MessageDB x : resultList){
+            System.out.println(x.ToString());
         }
     }
     }
