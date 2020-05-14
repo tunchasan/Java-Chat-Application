4ad9fc3169f0a383e5065be7b6abd7db03793255
tunchasan <hasantunc_ogr@outlook.com>
2020-05-14 11:31:28 +0300

    Added a class that handles all of message sending works

---
+++ Chat-App/src/chat/app/Server/MessageManager.java
@@ -0,0 +1,51 @@
+package chat.app.Server;
+
+import chat.app.Models.User;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class MessageManager {
+            
+   public static String serverResponseFormatter(String message){
+         return " [" + ChatServer.getServerTime() + " Server]  " + message;
+    }
+    
+    public static String clientMessageFormatter(String message, String senderName) {
+         return " [" + ChatServer.getServerTime() + " " + senderName + "]  " +  message;
+     }
+    
+    public static void messageSenderAsServer(PrintWriter writer, String message) {
+        writer.println(serverResponseFormatter(message));
+    }
+    
+    public static void messageSenderAsClient(PrintWriter writer, String message) {
+        //TODO
+    }
+    
+    //Group message
+    public static void sendGroupMessage(String message, User user) {
+        for(User person : user.getGroup().getGroupUsers()) {
+            
+            String finalMessage = clientMessageFormatter(message, "(Group " + user.getGroup().getGroupName() + ") "+ user.getName());
+            
+            person.getWriter().println(finalMessage);
+        }
+    }
+    // Broadcast message
+    public static void sendBroadcastMessage(String message, User user){
+         for (User person : ChatServer.getServerUserList()) {
+             
+            String finalMessage = MessageManager.clientMessageFormatter(message, user.getName());
+            
+            person.getWriter().println(finalMessage);
+         }
+    }
+    //Private message
+    public static void sendPrivateMessage(String message, User receiver, User sender){
+         String finalMessage = clientMessageFormatter(message, sender.getName());
+         
+         receiver.getWriter().println(finalMessage);
+    }
+}
+        
+    
